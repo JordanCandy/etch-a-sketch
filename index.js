9e84ecec1231fbc20e41e7310d30c55de8336fb7
Jordan Candy <jordan.candy07@gmail.com>
2023-04-16 11:29:07 +0200

    Add different input method
    Used input type range instead of prompt or input type text

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const container = document.querySelector("#container");
+const container = $("#container");
 function create(){
-            const gridItem = document.createElement("div");
-            gridItem.classList.add('content');
-            container.appendChild(gridItem);
+            const gridItem = $("div");
+            gridItem.addClass('content');
+            container.append("<div></div>");
 }
 
+const input = $("#firstInput");
+const viewVal = $("#myVal");
 
-function multiple(num){
-    num = Number(prompt("Number"));
-    for (var i = 0; i < num; i++){
-        for (var j = 0; j < num; j++){
-            if(num < 100){
-            create();
-            container.style.gridTemplateColumns = `repeat(${num}, auto)`;
-            container.style.gridTemplateRows = `repeat(${num}, auto)`;
-            }
-            else{
-                return "error";
-                break;
-            }
+$("input").on("input", function(){
+  var val = $(this).val();
+    viewVal.text(val);
+
+for (var i = 0; i < val; i++){
+    for (var j = 0; j < val; j++){
+        create();
+        container.css("grid-template-columns", `repeat(${val}, auto)`);
+        container.css("grid-template-rows", `repeat(${val}, auto)`);
     }
 }
-}
+});
 
+
+// function multiple(num){
+//      num = Number(prompt("Number: "));
+//     for (var i = 0; i < num; i++){
+//         for (var j = 0; j < num; j++){
+//             if(num < 100){
+//             create();
+//             container.css("grid-template-columns", `repeat(${num}, auto)`);
+//             container.css("grid-template-rows", `repeat(${num}, auto)`);
+            
+//             }
+//             else{
+//                 return "error";
+//                 break;
+//             }
+//     }
+// }
+// }
